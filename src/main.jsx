3f27e9fe91949f16b16b248801b991c976e07a5c
Negative-hub <and020702@gmail.com>
2022-12-25 19:46:19 +0300

    add github pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {Provider} from "react-redux";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import store from "./store/index.js";
 import './index.css'
 import RegistrationForm from "./components/RegistrationForm/index.jsx";
@@ -13,7 +13,7 @@ import NotFound from "./pages/NotFound.jsx";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={'/'} element={<App/>}>
             <Route path={'/'} element={<Home/>}/>
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Route path={'/login'} element={<LoginForm/>}/>
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
