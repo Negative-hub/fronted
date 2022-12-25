60db1c30fb0ad6e595b2f47516959344378e41ce
Negative-hub <and020702@gmail.com>
2022-12-25 20:07:28 +0300

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fronted/',
   server: {
     proxy: {
       '/api': {
