12fa2f6341f7f319fdbd8932801cb99561dbde6d
Negative-hub <and020702@gmail.com>
2022-12-28 02:41:21 +0300

    add filter in backend

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useCallback} from 'react'
+import React, {useEffect, useCallback, useState} from 'react'
 import {useDispatch, useSelector} from "react-redux";
 import {fetchTasks, createColumn} from "../store/tasks.slice.js";
 import Column from "../components/Column/index.jsx";
@@ -7,22 +7,28 @@ import './tasks.css'
 const Tasks = () => {
   const dispatch = useDispatch()
   const columns = useSelector((state) => state.tasks.tasks)
+  const [query, setQuery] = useState('')
 
   const addColumnHandler = useCallback(async () => {
     await dispatch(createColumn({title: 'Новая колонка', description: 'Новая колонка'}))
   }, [dispatch])
 
   useEffect(() => {
-    dispatch(fetchTasks())
-  }, [dispatch])
+    dispatch(fetchTasks({query: query.trim() ? query : undefined}))
+  }, [dispatch, query])
 
   return (
-      <div className='flex gap-x-2 overflow-y-scroll'>
-        {columns?.map(column => <Column key={column.id} column={column}/>)}
-        <Column>
-          <div className='plus' onClick={addColumnHandler}/>
-        </Column>
-      </div>
+      <>
+        <input type="text" value={query} onChange={e => setQuery(e.target.value)} className='input__query'/>
+
+        <div className='flex gap-x-2 overflow-y-scroll'>
+          {columns?.map(column => <Column key={column.id} column={column}/>)}
+          <Column>
+            <div className='plus' onClick={addColumnHandler}/>
+          </Column>
+        </div>
+      </>
+
   )
 }
 
